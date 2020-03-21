57a7708efcf81b9ef67b83556540fe197e2d324f
Porfirio <porfirio.tramontana@gmail.com>
2020-03-21 13:19:35 +0100

    Integrazione con AndroidCI

---
+++ app/src/androidTest/java/com/porfirio/orariprocida2011/activities/OrariProcida2011ActivityTest.java
@@ -0,0 +1,87 @@
+package com.porfirio.orariprocida2011.activities;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class OrariProcida2011ActivityTest {
+
+    @Rule
+    public ActivityTestRule<OrariProcida2011Activity> mActivityTestRule = new ActivityTestRule<>(OrariProcida2011Activity.class);
+
+    @Rule
+    public GrantPermissionRule mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.ACCESS_FINE_LOCATION",
+                    "android.permission.ACCESS_COARSE_LOCATION",
+                    "android.permission.INTERNET");
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+    @Test
+    public void orariProcida2011ActivityTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction textView = onView(
+                allOf(IsInstanceOf.<View>instanceOf(android.widget.TextView.class), withText("Uno Procida Residente"),
+                        childAtPosition(
+                                childAtPosition(
+                                        childAtPosition(
+                                                allOf(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
+                                                        childAtPosition(
+                                                                IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                                                0)),
+                                                0),
+                                        1),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Uno Procida Residente")));
+    }
+}
