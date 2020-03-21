05e350316c7351b74efc454b9a09132f8d1551c5
Porfirio <porfirio.tramontana@gmail.com>
2020-03-21 12:36:35 +0100

    Migrato verso AndroidX

--- app/build/generated/source/buildConfig/debug/com/porfirio/orariprocida2011/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/porfirio/orariprocida2011/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.porfirio.orariprocida2011";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 52;
+  public static final int VERSION_CODE = 53;
   public static final String VERSION_NAME = "1.2.4beta8";
 }
