340ef2f0aed692f243ab5de694ff3ed8f0180988
Rodolfo Diana <r.diana@studenti.unimol.it>
2025-01-20 17:52:52 +0100

    Update Firebase request for adapting at the new configuration of the date

--- app/src/main/java/com/porfirio/orariprocida2011/threads/transports/OnRequestTransportsDAO.java
+++ app/src/main/java/com/porfirio/orariprocida2011/threads/transports/OnRequestTransportsDAO.java
@@ -54,21 +54,16 @@ public class OnRequestTransportsDAO implements TransportsDAO {
 
     private Mezzo parseMezzo(DataSnapshot snapshot) {
         return new Mezzo(
-                snapshot.child("nomeNave").getValue(String.class),         // Nome nave
-                snapshot.child("oraPartenza").getValue(String.class),      // Ora partenza (ora)
-                snapshot.child("minutiPartenza").getValue(String.class),   // Ora partenza (minuti)
-                snapshot.child("oraArrivo").getValue(String.class),        // Ora arrivo (ora)
-                snapshot.child("minutiArrivo").getValue(String.class),     // Ora arrivo (minuti)
-                snapshot.child("portoPartenza").getValue(String.class),    // Porto di partenza
-                snapshot.child("portoArrivo").getValue(String.class),      // Porto di arrivo
-                snapshot.child("giorniInizioEsclusione").getValue(String.class), // Giorni inizio esclusione
-                snapshot.child("meseInizioEsclusione").getValue(String.class),   // Mese inizio esclusione
-                snapshot.child("annoInizioEsclusione").getValue(String.class),   // Anno inizio esclusione
-                snapshot.child("giorniFineEsclusione").getValue(String.class),   // Giorni fine esclusione
-                snapshot.child("meseFineEsclusione").getValue(String.class),     // Mese fine esclusione
-                snapshot.child("annoFineEsclusione").getValue(String.class),     // Anno fine esclusione
-                snapshot.child("giorniSettimana").getValue(String.class)         // Giorni della settimana
+            snapshot.child("nomeNave").getValue(String.class),
+            snapshot.child("oraPartenza").getValue(String.class),
+            snapshot.child("oraArrivo").getValue(String.class),
+            snapshot.child("inizioEsclusione").getValue(String.class),
+            snapshot.child("fineEsclusione").getValue(String.class),
+            snapshot.child("portoPartenza").getValue(String.class),
+            snapshot.child("portoArrivo").getValue(String.class),
+            snapshot.child("giorniSettimana").getValue(String.class)
         );
     }
+
 
 }
