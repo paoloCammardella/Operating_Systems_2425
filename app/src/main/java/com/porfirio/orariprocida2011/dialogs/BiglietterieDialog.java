1dc7ae3d134ac950ad94d3ab0002dcf305fcd209
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-01-22 13:20:33 +0100

    Add companies to Firebase DB

--- app/src/main/java/com/porfirio/orariprocida2011/dialogs/BiglietterieDialog.java
+++ app/src/main/java/com/porfirio/orariprocida2011/dialogs/BiglietterieDialog.java
@@ -15,72 +15,44 @@ import com.porfirio.orariprocida2011.R;
 import com.porfirio.orariprocida2011.entity.Compagnia;
 
 public class BiglietterieDialog extends DialogFragment {
-	private Compagnia c;
+    private Compagnia c;
 
-	public BiglietterieDialog() {
-		// Empty constructor required for DialogFragment
-	}
+    public BiglietterieDialog() {
+        // Empty constructor required for DialogFragment
+    }
 
-	public void setCompagnia(Compagnia c) {
-		this.c = c;
-	}
+    public void setCompagnia(Compagnia c) {
+        this.c = c;
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.biglietterie, container);
 
-		View view = inflater.inflate(R.layout.biglietterie, container);
-		TextView pn1 = view.findViewById(R.id.pn1);
-		TextView pn2 = view.findViewById(R.id.pn2);
-		TextView pn3 = view.findViewById(R.id.pn3);
-		TextView pn4 = view.findViewById(R.id.pn4);
-		TextView pn5 = view.findViewById(R.id.pn5);
-		TextView pn6 = view.findViewById(R.id.pn6);
+        TextView[] phoneNumbersLabels = {
+                view.findViewById(R.id.pn1),
+                view.findViewById(R.id.pn2),
+                view.findViewById(R.id.pn3),
+                view.findViewById(R.id.pn4),
+                view.findViewById(R.id.pn5),
+                view.findViewById(R.id.pn6)
+        };
 
-		if (c == null) {
-			pn1.setText(getString(R.string.NoBiglietterie));
-		} else {
-			int s = c.nomeNumeroTelefono.size();
-			if (s >= 1) {
-				String text = c.nomeNumeroTelefono.get(0) + " : " + c.numeroTelefono.get(0);
-				pn1.setText(text);
-				Linkify.addLinks(pn1, Linkify.PHONE_NUMBERS);
-			}
-			if (s >= 2) {
-				final String text = c.nomeNumeroTelefono.get(1) + " : " + c.numeroTelefono.get(1);
-				pn2.setText(text);
-				Linkify.addLinks(pn2, Linkify.PHONE_NUMBERS);
-			}
-			if (s >= 3) {
-				final String text = c.nomeNumeroTelefono.get(2) + " : " + c.numeroTelefono.get(2);
-				pn3.setText(text);
-				Linkify.addLinks(pn3, Linkify.PHONE_NUMBERS);
-			}
-			if (s >= 4) {
-				final String text = c.nomeNumeroTelefono.get(3) + " : " + c.numeroTelefono.get(3);
-				pn4.setText(text);
-				Linkify.addLinks(pn4, Linkify.PHONE_NUMBERS);
-			}
-			if (s >= 5) {
-				final String text = c.nomeNumeroTelefono.get(4) + " : " + c.numeroTelefono.get(4);
-				pn5.setText(text);
-				Linkify.addLinks(pn5, Linkify.PHONE_NUMBERS);
-			}
-			if (s >= 6) {
-				final String text = c.nomeNumeroTelefono.get(5) + " : " + c.numeroTelefono.get(5);
-				pn6.setText(text);
-				Linkify.addLinks(pn6, Linkify.PHONE_NUMBERS);
-			}
-		}
-		Button btnBack = view.findViewById(R.id.btnBack);
-		btnBack.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				dismiss();
-			}
-		});
+        if (c == null) {
+            phoneNumbersLabels[0].setText(getString(R.string.NoBiglietterie));
+        } else {
+            int s = Math.min(c.getContactsCount(), phoneNumbersLabels.length);
 
-		return view;
-	}
+            for (int i = 0; i < s; i++) {
+                phoneNumbersLabels[i].setText(c.getContactName(i) + ": " + c.getContactNumber(i));
+                Linkify.addLinks(phoneNumbersLabels[i], Linkify.PHONE_NUMBERS);
+            }
+        }
+
+        Button btnBack = view.findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> dismiss());
+
+        return view;
+    }
 
 }
--- app/src/main/java/com/porfirio/orariprocida2011/threads/companies/OnRequestCompaniesDAO.java
+++ app/src/main/java/com/porfirio/orariprocida2011/threads/companies/OnRequestCompaniesDAO.java
@@ -1,78 +1,75 @@
 package com.porfirio.orariprocida2011.threads.companies;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.porfirio.orariprocida2011.entity.Compagnia;
 
 import java.util.ArrayList;
 
 public class OnRequestCompaniesDAO implements CompaniesDAO {
 
+    private static final String DATABASE_TAG = "companies";
+
     private final MutableLiveData<CompaniesUpdate> update;
+    private final DatabaseReference database;
 
     public OnRequestCompaniesDAO() {
         this.update = new MutableLiveData<>();
+        this.database = FirebaseDatabase.getInstance().getReference(DATABASE_TAG);
     }
 
     public void requestUpdate() {
-        ArrayList<Compagnia> companies = new ArrayList<>();
+        database.addListenerForSingleValueEvent(new ValueEventListener() {
 
-        Compagnia c = new Compagnia("Caremar");
-        c.addTelefono("Napoli (Molo Beverello)", "0815513882");
-        c.addTelefono("Pozzuoli", "0815262711");
-        c.addTelefono("Pozzuoli", "0815261335");
-        c.addTelefono("Ischia", "081984818");
-        c.addTelefono("Ischia", "081991953");
-        c.addTelefono("Procida", "0818967280");
-        companies.add(c);
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                try {
+                    ArrayList<Compagnia> companies = new ArrayList<>();
 
-        c = new Compagnia("Gestur");
-        c.addTelefono("Sede", "0818531405");
-        c.addTelefono("Procida", "0818531405");
-        c.addTelefono("Pozzuoli", "0815268165");
-        companies.add(c);
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren())
+                        companies.add(parse(snapshot));
 
-        c = new Compagnia("SNAV");
-        c.addTelefono("Call Center", "0814285111");
-        c.addTelefono("Napoli", "0814285111");
-        c.addTelefono("Ischia", "081984818");
-        c.addTelefono("Procida", "0818969975");
-        companies.add(c);
+                    update.postValue(new CompaniesUpdate(companies));
+                } catch (Exception e) {
+                    update.postValue(new CompaniesUpdate(e));
+                }
+            }
 
-        c = new Compagnia("Medmar");
-        c.addTelefono("Napoli", "0813334411");
-        c.addTelefono("Procida", "0818969594");
-        c.addTelefono("Procida", "0818969190");
-        companies.add(c);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                update.postValue(new CompaniesUpdate(databaseError.toException()));
+            }
 
-        c = new Compagnia("Ippocampo");
-        c.addTelefono("Procida", "3663575751");
-        c.addTelefono("Procida", "0818967764");
-        c.addTelefono("Monte di Procida", "3397585125");
-        companies.add(c);
-
-        c = new Compagnia("Scotto Line");
-        c.addTelefono("Procida", "3343525753");
-        c.addTelefono("Procida", "0818968753");
-        c.addTelefono("Procida", "3394775523");
-        companies.add(c);
-
-        c = new Compagnia("LazioMar");
-        c.addTelefono("Napoli", "0771700604");
-        companies.add(c);
-
-        c = new Compagnia("Alilauro");
-        c.addTelefono("Napoli", "0814972252");
-        c.addTelefono("Call Center", "0814972222");
-        companies.add(c);
-
-        update.postValue(new CompaniesUpdate(companies));
+        });
     }
 
     @Override
     public LiveData<CompaniesUpdate> getUpdate() {
         return update;
+    }
+
+    private Compagnia parse(DataSnapshot snapshot) {
+        Compagnia company = new Compagnia(snapshot.getKey(), snapshot.child("name").getValue(String.class));
+
+        if (snapshot.hasChild("contacts")) {
+            DataSnapshot contacts = snapshot.child("contacts");
+
+            for (DataSnapshot contact : contacts.getChildren()) {
+                String name = contact.child("name").getValue(String.class);
+
+                for (DataSnapshot number : contact.child("numbers").getChildren())
+                    company.addContact(name, number.getValue(String.class));
+            }
+        }
+
+        return company;
     }
 
 }
