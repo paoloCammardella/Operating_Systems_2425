75ae20f307971809e861ed98c039e69adf5c7290
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-01-26 22:38:11 +0100

    Removed ScheduledWeatherDAO

--- app/src/main/java/com/porfirio/orariprocida2011/threads/weather/experimental/WeatherService.java
+++ app/src/main/java/com/porfirio/orariprocida2011/threads/weather/experimental/WeatherService.java
@@ -6,10 +6,17 @@ import android.os.Binder;
 import android.os.IBinder;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
+import com.porfirio.orariprocida2011.entity.Osservazione;
+import com.porfirio.orariprocida2011.threads.weather.WeatherAPI;
 import com.porfirio.orariprocida2011.threads.weather.WeatherDAO;
 import com.porfirio.orariprocida2011.threads.weather.WeatherUpdate;
 
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class WeatherService extends Service implements WeatherDAO {
@@ -24,7 +31,10 @@ public class WeatherService extends Service implements WeatherDAO {
     }
 
     private final IBinder binder = new LocalBinder();
-    private final ScheduledWeatherDAO weatherDAO = new ScheduledWeatherDAO();
+
+    private final MutableLiveData<WeatherUpdate> updates = new MutableLiveData<>();
+    private ScheduledExecutorService service;
+    private ScheduledFuture<?> future;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -34,18 +44,29 @@ public class WeatherService extends Service implements WeatherDAO {
     @Override
     public void onCreate() {
         super.onCreate();
-        weatherDAO.start(1, TimeUnit.HOURS);
+
+        service = Executors.newSingleThreadScheduledExecutor();
+        future = service.scheduleWithFixedDelay(() -> {
+            try {
+                List<Osservazione> forecasts = WeatherAPI.getForecasts();
+                updates.postValue(new WeatherUpdate(forecasts));
+            } catch (Exception e) {
+                updates.postValue(new WeatherUpdate(e));
+            }
+        }, 0, 1, TimeUnit.HOURS);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        weatherDAO.close();
+
+        future.cancel(false);
+        service.shutdown();
     }
 
     @Override
     public LiveData<WeatherUpdate> getUpdates() {
-        return weatherDAO.getUpdates();
+        return updates;
     }
 
 }
